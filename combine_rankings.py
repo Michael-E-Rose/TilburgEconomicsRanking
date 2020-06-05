b446d2d29c1146d48c57826f8fc61bce6ba6e7f1
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2020-06-05 14:03:02 +0200

    Add 2018 and 2019 rankings; refactor script

--- combine_rankings.py
+++ combine_rankings.py
@@ -26,11 +26,12 @@ def read_dataframes(files):
     for file in files:
         year = os.path.splitext(os.path.basename(file))[0][-4:]
         df = pd.read_csv(file, index_col=1)
-        df.columns = pd.MultiIndex.from_product(
-            [list(df.columns), [year]], names=['type', 'year'])
+        df.columns = pd.MultiIndex.from_product([list(df.columns), [year]],
+                                                names=['type', 'year'])
         dups = df.index.duplicated()
-        if dups.sum() > 0:
-            print("... {} from {}".format(dups.sum(), year))
+        if dups.sum():
+            print(f"... {dups.sum()} from {year}")
+            print('; '.join(df[dups].reset_index()['University'].tolist()))
         df = df[~dups]
         try:
             out = out.merge(df, "outer", left_index=True, right_index=True)
@@ -55,20 +56,20 @@ def save_melted(df, fname):
 
 def main():
     for folder in list(os.walk(SOURCE_FOLDER))[0][1]:
-        files = sorted(glob("{}{}/*.csv".format(SOURCE_FOLDER, folder)))
+        files = sorted(glob(f"{SOURCE_FOLDER}{folder}/*.csv"))
 
         df = read_dataframes(files)
 
         # Non-interpolated version
-        save_melted(df, "{}{}.csv".format(TARGET_FOLDER, folder))
+        save_melted(df, fname=f"{TARGET_FOLDER}{folder}.csv")
 
         # Interpolated version
         df = pd.concat([interpolate(df, 'Rank'), interpolate(df, 'Score')],
                        axis=1)
-        save_melted(df, "{}{}_interpolated.csv".format(TARGET_FOLDER, folder))
+        save_melted(df, fname=f"{TARGET_FOLDER}{folder}_interpolated.csv")
 
-        print(">>> Saving ranks for {} Economics Schools for weighting "
-              "according to {}".format(df.shape[0], folder.replace("_", " ")))
+        print(f">>> Saving ranks for {df.shape[0]:,} Economics Schools for "
+              f"weighting according to {folder.replace('_', ' ')}")
 
 
 if __name__ == '__main__':
